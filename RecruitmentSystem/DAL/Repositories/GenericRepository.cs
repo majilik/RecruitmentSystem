00c7e33dd0654de32e459deba67e8476b33b0215
syncberry <marcus.ording@hotmail.com>
2016-02-08 01:36:10 +0100

    implementations of repository interfaces

---
+++ RecruitmentSystem/DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using RecruitmentSystem.DAL.Interfaces;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace RecruitmentSystem.DAL
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity>
+          where TEntity : class
+    {
+        internal DbContext _context;
+        internal readonly DbSet<TEntity> _dbset;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbset = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> GetAll()
+        {
+            return _dbset.AsEnumerable<TEntity>();
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            _dbset.Add(entity);
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            _dbset.Remove(entity);
+        }
+
+        public virtual void Edit(TEntity entity)
+        {
+            _context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+        }
+
+        public virtual void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ RecruitmentSystem/DAL/Repositories/PersonRepository.cs
@@ -0,0 +1,81 @@
+﻿using RecruitmentSystem.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using RecruitmentSystem.Models;
+using System.Data.Entity;
+
+namespace RecruitmentSystem.DAL.Repositories
+{
+    public class PersonRepository : IPersonRepository, IDisposable
+    {
+        private RecruitmentContext _context;
+
+        public PersonRepository(RecruitmentContext context)
+        {
+            _context = context;
+        }
+
+        public virtual void Delete(long id)
+        {
+            Delete(_context.Persons.Find(id));
+        }
+
+        public virtual void Delete(Person person)
+        {
+            if (_context.Entry(person).State == EntityState.Detached)
+            {
+                _context.Persons.Attach(person);
+            }
+            _context.Persons.Remove(person);
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            return _context.Persons;
+        }
+
+        public Person GetByID(long id)
+        {
+            return _context.Persons.Find(id);
+        }
+
+        public void Insert(Person person)
+        {
+            _context.Persons.Add(person);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(Person person)
+        {
+            _context.Persons.Attach(person);
+            _context.Entry(person).State = EntityState.Modified;
+        }
+
+        #region IDisposable Support
+        private bool _disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing && _context != null)
+                {
+                    _context.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
