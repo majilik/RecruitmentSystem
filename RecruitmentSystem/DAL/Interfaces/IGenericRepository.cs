13ffdd53a8025bd023cc3fc0d3d1b7aa62b90f4e
syncberry <marcus.ording@hotmail.com>
2016-02-08 01:36:42 +0100

    illustrating usage of repository interfaces

---
+++ RecruitmentSystem/DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RecruitmentSystem.DAL.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        void Add(TEntity entity);
+        void Delete(TEntity entity);
+        void Edit(TEntity entity);
+        void Save();
+    }
+}
---
+++ RecruitmentSystem/DAL/Interfaces/IPersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using RecruitmentSystem.Models;
+using System;
+using System.Collections.Generic;
+
+namespace RecruitmentSystem.DAL.Interfaces
+{
+    public interface IPersonRepository : IDisposable
+    {
+        IEnumerable<Person> GetAll();
+        Person GetByID(long id);
+        void Insert(Person person);
+        void Delete(long id);
+        void Delete(Person person);
+        void Update(Person person);
+        void Save();
+    }
+}
