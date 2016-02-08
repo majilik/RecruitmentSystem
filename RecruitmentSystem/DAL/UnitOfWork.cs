7ba03a27c392e67662a607d2e677ba82f2eea3f0
syncberry <marcus.ording@hotmail.com>
2016-02-08 01:37:39 +0100

    illustrating usage of unit of work pattern

---
+++ RecruitmentSystem/DAL/UnitOfWork.cs
@@ -0,0 +1,51 @@
+﻿using RecruitmentSystem.Models;
+using System;
+
+namespace RecruitmentSystem.DAL
+{
+    public class UnitOfWork : IDisposable
+    {
+        private RecruitmentContext _context = new RecruitmentContext();
+        private GenericRepository<Person> _personRepository;
+
+        public GenericRepository<Person> PersonRepository
+        {
+            get
+            {
+                if (_personRepository == null)
+                {
+                    _personRepository = new GenericRepository<Person>(_context);
+                }
+                return _personRepository;
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        #region IDisposable Support
+        private bool _disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing && _context != null)
+                {
+                    _context.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
