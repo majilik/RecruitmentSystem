b124efa187114d79f7a19e994fdcf61a70774ad0
majilik <majilik@live.se>
2016-02-11 15:44:07 +0100

    Auth

---
+++ RecruitmentSystem/Controllers/AuthenticationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RecruitmentSystem.DAL;
+using RecruitmentSystem.DAL.Authentication;
+using RecruitmentSystem.Models;
+
+namespace RecruitmentSystem.Controllers
+{
+    [PersonAuthorization]
+    public class AuthenticationController : Controller
+    {
+        private RecruitmentContext db = new RecruitmentContext();
+        
+        [HttpPost]
+        public ActionResult Login() {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RecruitmentSystem/DAL/Authorization/PersonAuthorizationAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecruitmentSystem.DAL.Authentication
+{
+    public class PersonAuthorizationAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            return false;
+        }
+    }
+}
