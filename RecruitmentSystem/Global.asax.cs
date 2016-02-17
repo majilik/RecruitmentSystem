5412cd3b805fb93e7d88457428235b2fcb563840
syncberry <marcus.ording@hotmail.com>
2016-02-17 01:32:16 +0100

    Added exception handling

--- RecruitmentSystem/Global.asax.cs
+++ RecruitmentSystem/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using NLog;
+﻿using Cassandra;
+using NLog;
 using NLog.Config;
 using RecruitmentSystem.Logging;
+using System;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -9,13 +11,14 @@ using System.Web.Routing;
 namespace RecruitmentSystem
 {
     /// <summary>
+    /// Represents the starting point where application setup oocurs.
     /// Defines the methods, properties and events common to all objects in
     /// this ASP.NET application. A list of the supported event handlers can be
     /// viewed here <see cref="https://msdn.microsoft.com/library/2027ewzw%28v=vs.100%29.aspx"/>.
     /// </summary>
     public class MvcApplication : HttpApplication
     {
-        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private static NLog.Logger logger = LogManager.GetCurrentClassLogger();
         private CassandraTarget _cassandraTarget =
             new CassandraTarget(new string[] { "localhost" }, "logging", "log_entries", 1, null);
 
@@ -37,13 +40,18 @@ namespace RecruitmentSystem
 
         private void CassandraLoggerSetup()
         {
-            ConfigurationItemFactory.Default.Targets
-                .RegisterDefinition("Cassandra", typeof(CassandraTarget));
+            try {
+                ConfigurationItemFactory.Default.Targets
+                    .RegisterDefinition("Cassandra", typeof(CassandraTarget));
 
-            LogManager.Configuration.LoggingRules
-                .Add(new LoggingRule("*", LogLevel.Trace, _cassandraTarget));
+                LogManager.Configuration.LoggingRules
+                    .Add(new LoggingRule("*", LogLevel.Trace, _cassandraTarget));
 
-            LogManager.ReconfigExistingLoggers();
+                LogManager.ReconfigExistingLoggers();
+            } catch (NotSupportedException ex)
+            {
+                logger.Error(ex, "No Cassandra nodes available, proceeding with local file backup.");
+            }
         }
     }
 }
