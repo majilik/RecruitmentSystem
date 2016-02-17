b50c3e42d24da7c1326ee9eb0344e70694e40900
syncberry <marcus.ording@hotmail.com>
2016-02-17 10:17:34 +0100

    Extension to validate input arguments

---
+++ RecruitmentSystem/Extensions/ArgumentCheck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecruitmentSystem.Extensions
+{
+    public static class ArgumentCheck
+    {
+        public static string ThrowIfNullOrWhiteSpace(this String arg)
+        {
+            if (string.IsNullOrWhiteSpace(arg))
+                throw new ArgumentException("Argument must not be null, empty or white space.");
+
+            return arg;
+        }
+    }
+}
