0e82c7c12943339da36a995791509e982c4c6364
majilik <majilik@live.se>
2016-02-24 16:02:31 +0100

    Bugfix: Authorization now works without roles set. Bugfix: Unauthorized page is now translated correctly.

--- RecruitmentSystem/Controllers/HomeController.cs
+++ RecruitmentSystem/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Web.Mvc;
-using RecruitmentSystem.Resources;
 using System.Globalization;
 using RecruitmentSystem.Controllers.Base;
 using System.Web;
@@ -19,29 +18,5 @@ namespace RecruitmentSystem.Controllers
             return View();
         }
 
-        /// <summary>
-        /// Changes the locale. Defaults to EN_US.
-        /// Assumes route data 'locale' as a Locales enum.
-        /// </summary>
-        /// <returns>Redirects to Home.</returns>
-        public ActionResult ChangeLocale(Locales? locale)
-        {
-            //TODO: Reference from http://afana.me/post/aspnet-mvc-internationalization.aspx in Architecture Document
-            //NOTE: Locale data is saved in cookie '_locale'.
-            string localeName = LocalesExtension.ParseCultureInfo(locale).Name;
-
-            HttpCookie cookie = Request.Cookies["_locale"];
-            if (cookie != null)
-                cookie.Value = localeName;   // update cookie value
-            else
-            {
-                cookie = new HttpCookie("_locale");
-                cookie.Value = localeName;
-                cookie.Expires = DateTime.Now.AddYears(1);
-            }
-            Response.Cookies.Add(cookie);
-            
-            return RedirectToAction("Index");
-        }
     }
 }
